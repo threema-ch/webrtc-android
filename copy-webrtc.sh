b5d3337f2b60621e45439f7fce5a71f478de893a
Danilo Bargen <mail@dbrgn.ch>
2019-04-01 10:12:27 +0200

    Improve copy-webrtc.sh (#1)

--- copy-webrtc.sh
+++ copy-webrtc.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+set -euo pipefail
 
-SRC=~/Projects/webrtc-build-docker/out
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <path-to-webrtc-build>"
+    exit 1
+fi
+
+SRC=${1%/}
 DST=libs
+ARCHS=("arm:armeabi-v7a" "arm64:arm64-v8a" "x86:x86" "x64:x86_64")
 
 echo "Copying..."
-
-cp $SRC/libwebrtc.jar $DST/
-cp $SRC/arm/libjingle_peerconnection_so.so $DST/armeabi-v7a/libjingle_peerconnection_so.so
-cp $SRC/arm64/libjingle_peerconnection_so.so $DST/arm64-v8a/libjingle_peerconnection_so.so
-cp $SRC/x86/libjingle_peerconnection_so.so $DST/x86/libjingle_peerconnection_so.so
-cp $SRC/x64/libjingle_peerconnection_so.so $DST/x86_64/libjingle_peerconnection_so.so
+cp -v "$SRC/libwebrtc.jar" "$DST/"
+for arch in "${ARCHS[@]}"; do
+    IFS=: read -ra parts <<< "$arch"
+    cp -v "$SRC/${parts[0]}/libjingle_peerconnection_so.so" "$DST/${parts[1]}/libjingle_peerconnection_so.so"
+done
 
 echo "Done."
