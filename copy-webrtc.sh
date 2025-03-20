cf5127778ce412c7afc2927d221999fa1492ae42
Lenny <lennart.grahl@threema.ch>
2025-03-20 21:57:43 +0100

    Fix the copy script (wtf)

--- copy-webrtc.sh
+++ copy-webrtc.sh
@@ -6,15 +6,17 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-SRC=${1%/}
-DST=libs
-ARCHS=("arm:armeabi-v7a" "arm64:arm64-v8a" "x86:x86" "x64:x86_64")
+source=${1%/}
+destination=./libs
+architectures=("arm:armeabi-v7a" "arm64:arm64-v8a" "x86:x86" "x64:x86_64")
 
 echo "Copying..."
-cp -v "$SRC/libwebrtc.jar" "$DST/"
-for arch in "${ARCHS[@]}"; do
-    IFS=: read -ra parts <<< "$arch"
-    cp -v "$SRC/${parts[0]}/libjingle_peerconnection_so.so" "$DST/${parts[1]}/libjingle_peerconnection_so.so"
+mkdir -p "$destination"
+cp -v "$source/libwebrtc.jar" "$destination/"
+for architecture in "${architectures[@]}"; do
+    IFS=: read -ra parts <<< "$architecture"
+    mkdir -p "$destination/${parts[1]}"
+    cp -v "$source/${parts[0]}/libjingle_peerconnection_so.so" "$destination/${parts[1]}/libjingle_peerconnection_so.so"
 done
 
 echo "Done."
